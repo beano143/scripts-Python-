aebfb1452326bdaf0496e921fb14e043f34c4859
beano143 <143224038+beano143@users.noreply.github.com>
2023-10-18 15:19:38 -0500

    Create mass_shutdown.py

---
+++ minor_code/mass_shutdown.py
@@ -0,0 +1,19 @@
+import subprocess
+
+print("you are pc: ")
+z = subprocess.check_output(["hostname"]).decode().strip()
+print(z)
+
+def shutdown():
+    shutdown_hostnames = input("Enter the hostname of the target computer: ")
+    x = int(input("what is the start of your effect zone: "))
+    y = int(input("what is the end of your effect zone: "))
+    for i in range(x, (y+1)):
+        shutdown = shutdown_hostnames + str(i)
+        if shutdown == z:
+            print("found you")
+        else:
+            print("hit")
+            subprocess.call(["C:\\Windows\\System32\\shutdown.exe", "/s", "/m", shutdown, "/t", "0"])
+
+shutdown()
