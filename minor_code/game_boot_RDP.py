eac9f162e0fb71b09a2f42ae32672762f8b4c37b
Hemlock Valor <143224038+beano143@users.noreply.github.com>
2023-11-03 20:24:26 -0500

    Update game_boot_RDP.py

--- minor_code/game_boot_RDP.py
+++ minor_code/game_boot_RDP.py
@@ -1,4 +1,6 @@
 import os
+import subprocess
+from pydrop import RDPClient 
 
 def boot_game():
     # Replace the path with your Steam installation path if it's different
@@ -13,21 +15,37 @@ def boot_game():
     # Launch VRChat through Steam
     os.system(command)
 
-def connect():
+def find_from_mac(mac):
+    cmd = ["ping", mac, "-c", "1"]
+    ip_out = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    ip_out = ip_out.strip().split("\n")
+    # gets thee reply lines 
+    for line in ip_out:
+        output = line if "from" in line
+
+    # seperates out the ip addr from the output
+    output = output.strip().split("from")
+    output = output[-1].split(": ")
+    print(f"ip from mac addr: {output[0]}")
+    return output[0]
+
+
+def connect(ip_addr, user, passwd):
     from pyrdp import RDPClient
 
     # Set up the RDP client
-    client = RDPClient("hostname_or_ip", username="your_username", password="your_password")
+    client = RDPClient(ip_addr, username=user, password=passwd)
 
     # Connect to the remote desktop
     client.connect()
-
     # Perform actions on the remote desktop
-    # ...
+    boot_game()
 
-    # Disconnect from the remote desktop
-    client.disconnect()
+# defines username pass mac addr
+mac_addr = ""
+user_name = ""
+pass_word = ""
 
-# Call the functions
-boot_game()
-connect()
+# calls for the functions 
+ip = find_ip_from_mac(mac_addr)
+connect(ip, user_name, pass_word)
