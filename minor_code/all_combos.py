28e9e2df2f6955a43edaa24ed48e40c1e17fd648
Hemlock Valor <143224038+beano143@users.noreply.github.com>
2024-01-10 15:56:45 -0600

    Update all_combos.py

--- minor_code/all_combos.py
+++ minor_code/all_combos.py
@@ -1,8 +1,8 @@
-import string
+from string import ascii_letters, digits, puntuation
 from itertools import product
 
 def generate_combinations(size):
-    all_characters = string.ascii_letters + string.digits + string.punctuation
+    all_characters = ascii_letters + digits + punctuation
     with open(f'words_longform_{size}.txt', 'a') as file:
         for combo in product(all_characters, repeat=size):
             file.write(''.join(combo) + '\n')
